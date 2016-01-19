f1fa5a13839d49145c3990c53d298bd9f04ca410
Fareez Ahamed <fareez.ahamed@gmail.com>
2016-01-19 12:45:25 +0530

    Created directory structure and gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./public"
+    });
+
+    gulp.watch("scss/**/*.scss", ['sass']);
+    
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("public/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
+
+gulp.task('make',function(){
+	gulp.src('./node_modules/bootstrap-sass/assets/stylesheets/bootstrap/_variables.scss')
+		.pipe(gulp.dest('./scss'));
+});
